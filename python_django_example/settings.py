b24cf3f291457baf44254302f370157b4d4c1758
Chris Kemp <ckemp@salesforce.com>
2012-08-29 15:22:35 -0400

    init

--- python_django_example/settings.py
+++ python_django_example/settings.py
@@ -112,7 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'employees',
+    'python_django_example',
 )
 
 # A sample logging configuration. The only tangible logging
