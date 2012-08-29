153a32a947666e3f95fd5e96fe0290316a4a96aa
Chris Kemp <ckemp@salesforce.com>
2012-08-29 15:09:27 -0400

    init

--- python_django_example/models.py
+++ python_django_example/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Employee(models.Model):
-    id = models.IntegerField()
+    employeeId = models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
